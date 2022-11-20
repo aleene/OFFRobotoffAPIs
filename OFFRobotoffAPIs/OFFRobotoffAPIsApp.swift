998517f6ef5a35caec44e9ab61fb7121eb898909
Arnaud Leene <aleene@gmail.com>
2022-11-20 16:28:09 +0100

    Initial Commit

---
+++ OFFRobotoffAPIs/OFFRobotoffAPIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OFFRobotoffAPIsApp.swift
+//  OFFRobotoffAPIs
+//
+//  Created by Arnaud Leene on 20/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct OFFRobotoffAPIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
